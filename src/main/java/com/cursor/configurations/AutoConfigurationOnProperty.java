8071b5a1cdbf3a4fd81797820be8b5b24f6bda28
Juliya <ulcakopitko@gmail.com>
2021-01-25 20:17:55 +0200

    Change creating bean

--- src/main/java/com/cursor/configurations/AutoConfigurationOnProperty.java
+++ src/main/java/com/cursor/configurations/AutoConfigurationOnProperty.java
@@ -1,12 +1,16 @@
 package com.cursor.configurations;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(name = "custom.configuration.enabled", havingValue="true")
 public class AutoConfigurationOnProperty {
-    public AutoConfigurationOnProperty() {
-        System.out.println("AutoConfigurationOnProperty was created");
+
+    @Bean
+    @ConditionalOnProperty(name = "custom.configuration.enabled", havingValue="true")
+    public String beanOnProperty() {
+        System.out.println("AutoConfigurationOnProperty bean was created");
+        return "bean";
     }
 }
